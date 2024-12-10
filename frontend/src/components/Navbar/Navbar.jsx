fdf2b9815ce272eaa76a3437a89f2973a7b71d9b
ridimashakya <onlineridima@gmail.com>
2024-12-10 21:34:34 +0545

    added search engine optimization

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo11.png'
 import { Outlet, useNavigate } from 'react-router-dom'
@@ -6,16 +6,38 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const Navbar = () => {
 
-  const cart = useSelector((state) => state.cart ) ;
-
   const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart ) ;
 
   const header = [
     {link: "#crafts", title:"Crafts"},
     {link: "#about", title:"About"},
     {link: "#journal", title:"Journal"},
     {link: "#stories", title:"Stories"}
-  ]
+  ];
+
+  //for search engine optimization
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedCategory, setselectedCategory] = useState('All');
+
+  const categories = [
+    {name: 'Fiber Arts', path: '/fiberArts'},
+    {name: 'Visual Arts', path: '/visualArts'},
+    {name: 'Handcrafted Arts', path: '/handcraftedArts'},
+  ];
+
+  const handleSearch = () => {
+    if(selectedCategory){
+      const selectedPath = categories.find(
+        (category) => category.name === selectedCategory
+      )?.path;
+      if(selectedPath){
+        navigate(selectedPath, {state: {query: searchQuery}});
+      }
+    }else{
+      alert("Please select a category before searching");
+    }
+  }
 
   return (
     <>
@@ -27,8 +49,23 @@ const Navbar = () => {
         </ul>
 
         <ul className='right-nav'>
-          <li>Search</li>
-          <li onClick={()=>{navigate('/login')}}>Account</li>
+
+          <li>
+
+            <select  className='search-input' value={selectedCategory} onChange={(e) => setselectedCategory(e.target.value)}>
+              <option value=''>Search</option>
+              {categories.map((category, index) => (
+                <option key={index} value={category.name}>{category.name}</option>
+              ))}
+            </select>
+            
+            <button onClick={handleSearch} className='search-button'>
+              <i className='bx bx-search'></i>
+            </button>
+
+          </li>
+
+          <button><li onClick={()=>{navigate('/login')}}>Account</li></button>
 
           <div>
             <i className='bx bxs-cart text-2xl' onClick={() => navigate('/cart')}></i>
